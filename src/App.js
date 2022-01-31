e7b69bc7cd5ef11b81594e419f405efb9b048cfb
linhtran <linh.cntt097@gmail.com>
2022-02-01 00:00:18 +0700

    happy new year 2022

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"https://mrhsridgereview.org/wp-content/uploads/2021/01/2021-neon-signboard_73458-714.jpg"}  alt="logo" />
-        <h1 style={{ color:"red"}}>Happy New Year 2021 !</h1>
-          <h2>Linh.cntt097</h2>
-      </header>
-    </div>
-  );
+const  App = () =>  {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={"https://cms.lichngaytot.com/medias/standard/2021/12/10/--Cho-bo-lo-thang-may-man-duoc-ban-phuoc-lanh-cua-12-con-giap-nam-2022,-cua-cai-sieu-doi-dao------.jpg"}
+                    alt="logo"/>
+                <h1 style={{color: "green"}}>Happy New Year 2022 !</h1>
+                <h2 style={{color: 'indigo'}}>Linh.cntt097</h2>
+            </header>
+        </div>
+    );
 }
 
 export default App;
