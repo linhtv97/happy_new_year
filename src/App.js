f847f2394b934a6970cea421463c67a963a7ac43
linh.cntt097@gmail.com <linh.cntt097@gmail.com>
2024-02-10 00:05:52 +0700

    2024 many tasks, features,... and small bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const  App = () =>  {
                 <img
                     src={"https://cms.lichngaytot.com/medias/standard/2021/12/10/--Cho-bo-lo-thang-may-man-duoc-ban-phuoc-lanh-cua-12-con-giap-nam-2022,-cua-cai-sieu-doi-dao------.jpg"}
                     alt="logo"/>
-                <h1 style={{color: "green"}}>Happy New Year 2022 !</h1>
+                <h1 style={{color: "green"}}>Happy New Year 2024 !</h1>
                 <h2 style={{color: 'indigo'}}>Linh.cntt097</h2>
             </header>
         </div>
